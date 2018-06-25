973e1e88df331ee201476b156368a4d9f8e2aa01
cvmartins1996 <cvmartins1996@gmail.com>
2018-06-24 22:09:55 -0300

    teste

--- src/com/customer/io/configuration/ClassName.java
+++ src/com/customer/io/configuration/ClassName.java
@@ -4,6 +4,8 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.customer.io.core.CustomerDto;
+
 public class ClassName {
 	private Field[] declaredFields;
 	private List<String> fields = new ArrayList<>();
@@ -11,8 +13,8 @@ public class ClassName {
 	public List<String> to(Class<?> className, Long Id) {
 		try {
 			
-			className.newInstance();
-			className.getConstructor();
+			newInstance(className);
+			constructor(className);
 			declaredFields = className.getDeclaredFields();
 			
 			for (Field field : declaredFields) {
@@ -31,6 +33,51 @@ public class ClassName {
 		}
 		return fields;
 	}
+
+	private void constructor(Class<?> className) throws NoSuchMethodException {
+		className.getConstructor();
+	}
+
+	private void newInstance(Class<?> className) throws InstantiationException, IllegalAccessException {
+		className.newInstance();
+	}
+	
+	public static boolean set(Object object, String fieldName, Object fieldValue) {
+		Class<?> className = object.getClass();
+		
+		try {
+			Field declaredField = className.getDeclaredField(fieldName);
+			
+			declaredField.setAccessible(true);
+			declaredField.set(object, fieldValue);
+			
+			return true;
+		} catch (NoSuchFieldException | SecurityException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
 	
 }
 
+
+class Teste{
+	public static void main(String[] args) {
+		try {
+			Class<?> className = Class.forName(CustomerDto.class.getName());
+			Object instance = className.newInstance();
+			ClassName.set(instance, "id", 12);
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+	}
+}
